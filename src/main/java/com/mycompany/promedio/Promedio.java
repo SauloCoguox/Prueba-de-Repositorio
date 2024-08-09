ad0030fc0fcefcd35e0b948cd5cca24a2d20e862
SauloCoguox <jconergt@gmail.com>
2024-08-09 09:59:11 -0600

    Se agregan los comentarios

--- src/main/java/com/mycompany/promedio/Promedio.java
+++ src/main/java/com/mycompany/promedio/Promedio.java
@@ -1,14 +1,17 @@
 package com.mycompany.promedio;
 import java.util.Scanner;
+/*Esta es la libreria para poder utilizar el metodo Scannes 
 /**
- *
+ * 
  * @author coguox
  */
 public class Promedio {
 
     public static void main(String[] args) {
          Scanner Scanner = new Scanner(System.in);
+        /*Se crea la variable Scannes*/ 
         int prome, suma, a, b, c, d;
+        /*Se declaran las variables*/
         System.out.println("Ingrese las notas");
         System.out.println("Ingrese la primera nota");
         a = Scanner.nextInt();
@@ -20,6 +23,8 @@ public class Promedio {
         d = Scanner.nextInt();
         suma = a+b+c+d;
         prome = suma / 4;
+        /*Se realizan las operaciones para dar el promedio*/
         System.out.println("El promedio de las notas es de: "+ prome);
+        /*Se muestra el resultado*/
     }
 }
